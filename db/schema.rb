cd94cedcd59b4d83d93ec8d7199713c95c891a0a
Joé Dupuis <1518299+JoeDupuis@users.noreply.github.com>
2025-05-25 01:54:10 -0700

    Add role enum to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_25_021419) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_25_021550) do
   create_table "agents", force: :cascade do |t|
     t.string "name"
     t.string "docker_image"
@@ -69,6 +69,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_25_021419) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
