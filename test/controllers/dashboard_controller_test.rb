d5533be412a7f3bd6b275ca23068cb94a374866a
Joé Dupuis <1518299+JoeDupuis@users.noreply.github.com>
2025-05-24 18:40:42 -0700

    Add dashboard landing page

---
+++ test/controllers/dashboard_controller_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class DashboardControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
+  test "root requires authentication" do
+    get root_url
+    assert_redirected_to new_session_path
+
+    login @user
+    get root_url
+    assert_response :success
+    assert_match projects_path, @response.body
+  end
+end
