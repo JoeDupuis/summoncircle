d95e40d47e6893305659b367a467316c649f826d
Joé Dupuis <joe@dupuis.io>
2025-05-31 17:10:08 -0700

    Remove unnecessary migrations - back to original schema state
    - Delete make_project_id_nullable_in_tasks migration
    - Delete make_project_id_required_in_tasks migration
    - Update schema version back to 2025_05_31_221419
    - project_id was always required, no need for the round trip

--- db/migrate/20250531230009_make_project_id_nullable_in_tasks.rb
+++
@@ -1,5 +0,0 @@
-class MakeProjectIdNullableInTasks < ActiveRecord::Migration[8.0]
-  def change
-    change_column_null :tasks, :project_id, true
-  end
-end
--- db/migrate/20250531234908_make_project_id_required_in_tasks.rb
+++
@@ -1,5 +0,0 @@
-class MakeProjectIdRequiredInTasks < ActiveRecord::Migration[8.0]
-  def change
-    change_column_null :tasks, :project_id, false
-  end
-end
