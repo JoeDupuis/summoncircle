f94bf29eb3055bea3bacbb9b6ea4d2aac8c15b0d
Joé Dupuis <joe@dupuis.io>
2025-06-01 20:29:28 -0700

    Fix encryption configuration for CI environment
    Add test encryption keys to resolve missing credential errors in CI

---
+++ config/initializers/test_encryption.rb
@@ -0,0 +1,7 @@
+if Rails.env.test?
+  ActiveRecord::Encryption.configure(
+    primary_key:          '1c7e6aa54b2cfbf79cce0e3d82c34d51',
+    deterministic_key:    '5db7bb47b6394d1689d6a5c47d28fbcd',
+    key_derivation_salt:  '9e6f0e5b9e524fb3afa6077274baf432'
+  )
+end
