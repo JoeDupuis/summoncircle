dfdd1515bf5092b8599a9ff9478c569a71e06814
Joé Dupuis <joe@dupuis.io>
2025-06-01 17:18:04 -0700

    Remove unused vendorized JS packages and importmap pins
    Since we're using diff2html via CDN, we don't need the vendorized
    packages or importmap pins for diff2html, diff, and hogan.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "diff2html" # @3.4.51
-pin "diff" # @7.0.0
-pin "hogan.js" # @3.0.2
