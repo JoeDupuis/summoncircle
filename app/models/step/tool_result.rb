59a21b31cd7337fe836b4848a38de3f8f983d97b
Joé Dupuis <joe@dupuis.io>
2025-06-05 23:05:12 -0700

    Fix linting issues

--- app/models/step/tool_result.rb
+++ app/models/step/tool_result.rb
@@ -8,7 +8,7 @@ class Step::ToolResult < Step
   def link_to_tool_call
     return unless respond_to?(:tool_use_id) && tool_use_id.present?
 
-    tool_call_types = [Step::ToolCall] + Step::ToolCall.descendants
+    tool_call_types = [ Step::ToolCall ] + Step::ToolCall.descendants
     matching_tool_call = run.steps.where(
       type: tool_call_types.map(&:name),
       tool_use_id: tool_use_id
