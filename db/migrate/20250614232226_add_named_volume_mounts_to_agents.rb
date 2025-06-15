8cac73d83be2ab5742af28e5619c62bef0520b83
Joé Dupuis <joe@dupuis.io>
2025-06-14 17:05:45 -0700

    Remove unnecessary migrations
    These migrations were artifacts from exploring different approaches and are not needed for the final implementation.

--- db/migrate/20250614232226_add_named_volume_mounts_to_agents.rb
+++
@@ -1,5 +0,0 @@
-class AddNamedVolumeMountsToAgents < ActiveRecord::Migration[8.0]
-  def change
-    add_column :agents, :named_volume_mounts, :json, default: {}
-  end
-end
--- db/migrate/20250614232533_remove_unused_columns_from_agents.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUnusedColumnsFromAgents < ActiveRecord::Migration[8.0]
-  def change
-    remove_column :agents, :claude_config_volume_name, :string
-    remove_column :agents, :named_volume_mounts, :json
-  end
-end
