04b563ae625e77a957da5e65d4deaeb15d78f16e
Joé Dupuis <joe@dupuis.io>
2025-06-14 22:46:49 -0700

    Remove OAuth migration files and claude_oauth rake task
    - Delete migrations that added/removed oauth_credentials column
    - Remove entire claude_oauth.rake file with build_image and create_volume tasks
    - Clean up empty fixture comment

--- db/migrate/20250615012300_add_oauth_credentials_to_agents.rb
+++
@@ -1,5 +0,0 @@
-class AddOauthCredentialsToAgents < ActiveRecord::Migration[8.0]
-  def change
-    add_column :agents, :oauth_credentials, :text
-  end
-end
--- db/migrate/20250615020426_remove_oauth_credentials_from_agents.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOauthCredentialsFromAgents < ActiveRecord::Migration[8.0]
-  def change
-    remove_column :agents, :oauth_credentials, :text
-  end
-end
