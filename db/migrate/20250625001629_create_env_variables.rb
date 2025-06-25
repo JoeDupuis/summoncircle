ffbd46cbba03ffaa061aa37968d85643dc945a5a
Joé Dupuis <joe@dupuis.io>
2025-06-24 18:31:37 -0700

    Remove data migration from env_variables migration
    Since we're pre-release, we don't need to migrate existing data.

--- db/migrate/20250625001629_create_env_variables.rb
+++ db/migrate/20250625001629_create_env_variables.rb
@@ -9,22 +9,5 @@ class CreateEnvVariables < ActiveRecord::Migration[8.0]
     end
 
     add_index :env_variables, [ :envable_type, :envable_id, :key ], unique: true, name: 'index_env_vars_on_envable_and_key'
-
-    # Migrate existing env_variables JSON data to the new table
-    reversible do |dir|
-      dir.up do
-        Agent.find_each do |agent|
-          next unless agent.env_variables.present?
-
-          agent.env_variables.each do |key, value|
-            EnvVariable.create!(
-              envable: agent,
-              key: key,
-              value: value
-            )
-          end
-        end
-      end
-    end
   end
 end
